e4be5e3664409b8e0b061af196369ef95fc36a67
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2025-04-23 19:16:56 +0600

    copy

--- handlers/hello.go
+++ handlers/hello.go
@@ -2,29 +2,35 @@ package handlers
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// Hello is a simple handler
 type Hello struct {
 	l *log.Logger
 }
 
+// NewHello creates a new hello handler with the given logger
 func NewHello(l *log.Logger) *Hello {
 	return &Hello{l}
 }
 
-func (h *Hello) ServeHTTP(w http.ResponseWriter, r http.Request) {
-	h.l.Println("Hello World!")
-	d, err := io.ReadAll(r.Body)
+// ServeHTTP implements the go http.Handler interface
+// https://golang.org/pkg/net/http/#Handler
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Handle Hello requests")
+
+	// read the body
+	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Oops!", http.StatusBadRequest)
-		log.Printf("Error %s\n", err)
-	} else {
-		log.Printf("Data %s\n", d)
+		h.l.Println("Error reading body", err)
+
+		http.Error(rw, "Unable to read request body", http.StatusBadRequest)
+		return
 	}
 
-	fmt.Fprintf(w, "Hello %s\n", d)
-
+	// write the response
+	fmt.Fprintf(rw, "Hello %s", b)
 }
