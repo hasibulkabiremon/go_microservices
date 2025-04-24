0260981c619e350aeadfd2553b08c78c7febad06
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2025-04-24 15:08:00 +0600

    Fixed Goodbye url

--- handlers/goodbye.go
+++ handlers/goodbye.go
@@ -1,18 +1,26 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+// Goodbye is a simple handler
 type Goodbye struct {
 	l *log.Logger
 }
 
+// NewHGoodbye creates a new goodbye handler with the given logger
 func NewGoodbye(l *log.Logger) *Goodbye {
 	return &Goodbye{l}
 }
 
-func (g *Goodbye) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Byee"))
+// ServeHTTP implements the go http.Handler interface
+// https://golang.org/pkg/net/http/#Handler
+func (h *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Handle Goodbye request")
+
+	// write the response
+	fmt.Fprintf(rw, "Goodbye")
 }
