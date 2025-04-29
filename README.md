1325a191fd98ed04849901e0b2d800abb7677b09
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2025-04-29 16:42:24 +0600

    Json return in web

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go MicroService :RESTfull 
+## Struct data to json convert:
+### json.Marshal(--struct--) return []byte , err
---
+++ data/products.go
@@ -0,0 +1,39 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+func GetProducts() []*Product {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffe",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().Local().String(),
+		UpdatedOn:   time.Now().Local().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffe without milk",
+		Price:       1.99,
+		SKU:         "abc324",
+		CreatedOn:   time.Now().Local().String(),
+		UpdatedOn:   time.Now().Local().String(),
+	},
+}
