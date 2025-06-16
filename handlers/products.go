5c26370105897ca528e1192791e06b885eea077e
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2025-06-16 20:37:15 +0600

    Middleware has some issue

--- handlers/products.go
+++ handlers/products.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"e5/data"
 	"log"
 	"net/http"
@@ -17,23 +18,21 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) UpdateProducts(w http.ResponseWriter, r *http.Request) {
+func (p Products) UpdateProducts(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 	id, er := strconv.Atoi(vars["id"])
 	if er != nil {
 		http.Error(w, "Unable to find ID", http.StatusBadRequest)
+		return
 	}
-	prod := &data.Product{}
 
-	err := prod.FromJSON(r.Body)
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
-	if err != nil {
-		http.Error(w, "Unable to unmarshal Json", http.StatusBadRequest)
-	}
-	erp := data.UpdateProduct(id, prod)
+	err := data.UpdateProduct(id, &prod)
 
-	if erp == data.ErrProductNotFound || erp != nil {
+	if err == data.ErrProductNotFound || err != nil {
 		http.Error(w, "Product Not Found", http.StatusNotFound)
 		return
 	}
@@ -51,11 +50,31 @@ func (p *Products) AddProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
 }
-func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
+func (p Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Product")
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+
+		err := prod.FromJSON(r.Body)
+
+		if err != nil {
+			http.Error(w, "Unable to unmarshal Json", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	})
 }
